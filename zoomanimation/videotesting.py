0ac5b588f0a60deddece38a4aa63f547ecdc3cee
Ardnys <semresavas35@gmail.com>
2023-02-16 16:25:59 +0300

    added video saving

--- zoomanimation/videotesting.py
+++ zoomanimation/videotesting.py
@@ -26,20 +26,53 @@ class VideoThread(QtCore.QThread):
     videoupdate = QtCore.pyqtSignal(QtGui.QImage)
     def run(self) -> None:
         self.threadactive = True
-        camera = cv2.VideoCapture(0) # 0 is webcam local camera or smth
+        self._camera = cv2.VideoCapture(0) # 0 is webcam local camera or smth
+        
+        # parameters for output video
+        HEIGHT = 480
+        WIDTH = 640
+        FPS = 30.0
+
+        # set camera settings if needed
+
+        # self._camera.set(cv2.CAP_PROP_CONVERT_RGB , 1)
+        # self._camera.set(cv2.CAP_PROP_BUFFERSIZE, 100)
+        # self._camera.set(cv2.CAP_PROP_FPS, FPS)
+        # self._camera.set(3, WIDTH)
+        # self._camera.set(4, HEIGHT)
+        
+
+        fourcc = cv2.VideoWriter_fourcc(*'XVID') # XVID for .avi, mp4v for .mp4 format
+        # output parameters such as fps and size can be changed
+        self._output = cv2.VideoWriter("output.avi", fourcc, FPS, (WIDTH, HEIGHT))
+        # further frustration reading: https://stackoverflow.com/questions/30509573/writing-an-mp4-video-using-python-opencv
+
+        """
+        VideoCapture takes about 1.5 seconds to initialise
+        setting WIDTH, HEIGHT, and FPS takes about 3.7 seconds to set
+        settings everything takes 4.9 seconds to set
+        the video didn't work when i didn't do those but now it works without setting anything"""
+
         while self.threadactive:
-            okay, frame = camera.read()
+            okay, frame = self._camera.read()
             if okay:
                 # the type of frame is numpy array 
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 flippedimage = cv2.flip(image, 1)
+
+                # write the flipped frame without any processing
+                self._output.write(cv2.flip(frame, 1))
+
                 qtimage = QtGui.QImage(flippedimage.data, flippedimage.shape[1], flippedimage.shape[0], QtGui.QImage.Format_RGB888)
-                picture = qtimage.scaled(480, 360, QtCore.Qt.KeepAspectRatio)
+                picture = qtimage.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
                 self.videoupdate.emit(picture)
                       
     
     def stop(self):
         self.threadactive = False
+        # release the objects idk what they do
+        self._camera.release()
+        self._output.release()
         self.quit()
 
 
